beb2ba599f43629d57524725c697baad52a9d4ea
João Palmeiro <joaommpalmeiro@gmail.com>
2023-01-24 16:04:40 +0000

    Update dev.ts

--- dev.ts
+++ dev.ts
@@ -1,6 +1,6 @@
 import { genBookmarkHTML } from "./index";
 
-const content = [
+const BOOKMARKS_ONLY = [
     {
         href: "https://gitlab.com/joaommpalmeiro",
         name: "GitLab",
@@ -11,4 +11,31 @@ const content = [
     },
 ];
 
-console.log(genBookmarkHTML(content));
+const BOOKMARKS_IN_FOLDER = [
+    {
+        name: "Code",
+        children: BOOKMARKS_ONLY,
+    },
+];
+
+const FOLDERS_IN_FOLDER = [
+    {
+        name: "Profiles",
+        children: [
+            ...BOOKMARKS_IN_FOLDER,
+            {
+                name: "Social Media",
+                children: [
+                    {
+                        href: "https://ciberlandia.pt/@joaopalmeiro",
+                        name: "Mastodon",
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+console.log(genBookmarkHTML(BOOKMARKS_ONLY));
+console.log(genBookmarkHTML(BOOKMARKS_IN_FOLDER));
+console.log(genBookmarkHTML(FOLDERS_IN_FOLDER));
